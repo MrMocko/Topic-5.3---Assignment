c3c1a567842e3f3e098655f6847d24953d390a8f
Andre Mocko <339723967@GoTVDSB.ca>
2024-10-22 09:40:15 -0400

    Not done

--- Topic 5.3 - Assignment/Program.cs
+++ Topic 5.3 - Assignment/Program.cs
@@ -7,9 +7,9 @@ namespace Topic_5._3___Assignment
     {
         static void Main(string[] args)
         {
-            int age;
+            int age, guess;
             double money;
-            string isWorking;
+            string isWorking, password;
 
             Console.WriteLine("Hello firend this is OTZ and today i will be asking you what your age is, so how old are you?");
             Console.Write("Enter age here : ");
@@ -25,14 +25,47 @@ namespace Topic_5._3___Assignment
             Console.WriteLine("Press Enter to continue");
             Console.ReadLine();
             Console.Clear();
-            Console.WriteLine("Hey are you working somtime later?");
-            Console.Write("Enter your answer here :");
+            Console.WriteLine("Hey are you working sometime later?");
+            Console.Write("Enter your answer here: ");
             isWorking = Console.ReadLine();
-            Console.WriteLine("Oh ok cool i guess, and another question, how much money do you got?");
-            Console.Write("Enter your answer here :");
+            Console.WriteLine("Oh ok cool i guess, and another question, how much money you got?");
+            Console.Write("Enter your answer here: ");
             double.TryParse(Console.ReadLine(), out money);
+            if (isWorking.ToLower() == "no" && money >= 20)
+            {
+                Console.WriteLine("Ok sweet man lets go watch the new Spider-man movie at 6?");
+            }
+            else if (isWorking.ToLower() == "yes" && money < 20)
+            {
+                Console.WriteLine("AWWWWW MAAAAN BUT I WANNA WATCH THE NEW SPIDER-MAN MOVIE, why do you suck?");
+            }
+            else if (isWorking.ToLower() == "no" && money < 20)
+            {
+                Console.WriteLine("Man dude why are you broke?");
+            }
+            else if (isWorking.ToLower() == "yes" && money > 20)
+            {
+                Console.WriteLine("AWWWWWWW MAN THATS TERRIBLE, like paper lol :)");
+            }
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Hey man i dare you to guess this password correctly, if you do i'll give you a billion dollars");
+            Console.Write("Enter your answer here: ");
+            password = Console.ReadLine();
+            Console.WriteLine("How many guesses");
 
+            Int32.TryParse(Console.ReadLine(), out guess);
 
+            if (password.ToLower() == "santa" && guess < 5)
+            {
+                Console.WriteLine("WOWZA MAN YOU GOT IT I'M SUPER IMPRESSED");
+            }
+            else
+            {
+                Console.WriteLine("Oh too bad you suck LOL");
+            }
+            Console.WriteLine("");
         }
     }
 }
